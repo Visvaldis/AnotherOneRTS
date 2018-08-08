f8eb8c4951a6b242d26b62c65710323f0291a0a0
Alexandr <alexandr.job@yahoo.com>
2018-08-08 15:44:27 +0300

    Drawing rectangle in GUI
    Classes GUI_Manager.cs and static Utils.cs added

---
+++ Assets/Scripts/GUI_Manager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Manager : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        Utils.DrawScreenRect(new Rect(50, 50, 200, 200), Color.green);
+    }
+}
